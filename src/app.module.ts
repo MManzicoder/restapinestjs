b70c07b90693cc5b82bf94c163715e3b43045ab8
Mmanzi monnierey <mudahemukamanzi@yahoo.com>
2021-11-15 10:58:27 +0200

    added mongodb database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import StudentsModule from './students/students.module';
 import AuthModule from './auth/auth.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [StudentsModule, AuthModule],
+  imports: [StudentsModule, AuthModule, MongooseModule.forRoot("mongodb://localhost/nest")],
   controllers: [AppController],
   providers: [AppService],
 })
